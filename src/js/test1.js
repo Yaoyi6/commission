39473180754ca5792c01d8d83f2d4c6a3c0f4dd5
Antony-Bacilio <antonybaciliosalazar@gmail.com>
2018-01-16 17:40:08 +0100

    deleted

--- src/js/test1.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-      $.ajax({
-	   url:'connexion.php',
-	   success: function(data) {
-	      var html_text = data;
-	      $('#commissions').append(html_text);
-	  	}
-      });
-});
